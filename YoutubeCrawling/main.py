8055df961d3a96765a7dc9a46f9bab1eff28bb6d
kr_MATAGI <dev_matagi@kakao.com>
2023-07-14 11:06:28 +0900

    update comments

--- YoutubeCrawling/main.py
+++ YoutubeCrawling/main.py
@@ -31,6 +31,87 @@ from xpath_def import XPath
         - 한 댓글에 여러 사람이 댓글 다는 것과 같은 현상 방지 필요
 '''
 
+'''
+    Selenium 요소를 조작하는 것 중 많이 쓰일만한 것들
+        1. tag_name
+        2. text
+        3. back() / forward()
+        4. implicitly_wait(sec) : 최대 대기 시간을 초 단위로 지정해서 처리가 끝날때까지 대기
+        5. set_page_load_timeout() : 페이지를 읽는 타임아웃 시간을 지정
+        6. set_window_position(x, y, windowHandle), set_window_size(width, height)
+'''
+
+'''
+    WebDriverWait 관련 예제
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, "myDynamicElem"))
+        )
+    > WebDriverWait(driver, 10) -> 최대 10초동안 기다린다
+    > until(EC.presence_of_element_located((By.ID, "myDynamicElement"))) -> ID에 해당하는 elem이 표시될때까지 기다림
+'''
+
+'''
+    time.sleep(10) -> 페이지 로딩에 상관없이 무조건 10초를 기다리게 함.
+    driver.implicitly_wait(10)은 페이지가 2초만에 로딩된다면, 10초를 기다리지 않고 바로 다음 코드로 넘어간다.
+'''
+
+'''
+    Scroll 관련 동작
+        1. 지정한 위치로 스크롤 내리기
+            browser.execute_script("window.scrollTo(0, 900)")
+
+        2. 화면의 가장 아래로 스크롤 내리기
+            browser.execute_script("window.scrollTo(0, document.body.scrollHeight))
+'''
+
+'''
+    headless option
+        > 웹 브라우저를 열지 않고 크롤링을 진행할때 사용한다.
+        > headless 옵션을 사용할 경우, 서버측에서 접근을 차단하는 경우가 있기 때문에 option.add_argument를 통해
+         UserAgent 값을 설정해야 한다.
+'''
+
+'''
+    BeautifulSoup()
+        > HTML과 XML 문서들의 구문을 분석하기 위한 파이썬 패키지
+        > HTML로부터 데이터를 추출하기 위해 사용할 수 있는 파싱된 페이지의 parse tree를 만든다.
+        e.g.
+            soup = BeautifulSoup(arg_1, 'html.parser')
+
+        > head / body tag의 정보 가져오기
+            soup.head()
+            soup.body()
+
+        > 특정 태그 찾기 find()
+            soup.find('p') -> 처음 발견한 p 태그를 가져온다
+            soup.find_all('p') -> 모든 p 태그를 가져온다
+
+        > find() 옵션인 clss_, {key:values}를 이용해서 자세하게 찾기
+            soup.find('p', class_='inner-text first-item')
+            soup.find('p', {'class': 'inner-text first-item'})
+
+        > 태그 안에 있는 text만 가져오는 함수
+            soup.find('p').text
+            soup.find('p').string
+            soup.find('p').get_text()
+
+        > select_one(), select() 함수로 원하는 태그 찾기
+            soup.select('#exchangeList > li') -> #은 id값을 이용해서 찾을 때 사용
+            soup.select('.exchangeList > li') -> .은 class를 이용해서 찾을 때 사용
+'''
+
+'''
+    iframe이 웹에 있을 경우, html의 elem에 접근이 어려움
+    따라서 iframe에 먼저 접근해주고 크롤링을 해야한다.
+    e.g.
+        # iframe 태그로 이동
+        iframe = selenium.find_element_by_css_selector('#frame_ex1')
+
+        # 태그 이동
+        driver.switch_to_frame(iframe)
+
+'''
+
 LOAD_TIME_SLEEP = 2
 PROMPT_FORMAT = """
 아래 Youtube 댓글들을 참고해서 새로운 비슷한 의미를 갖는 댓글을 생성해줘
